eb96e4604f3c85d26569722cdb8fcaee107e970b
Kamil Ziecik <kamil.ziecik1999@gmail.com>
2021-01-25 20:21:46 +0100

    next commit

---
+++ webpack.config.js
@@ -0,0 +1,116 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+const env = process.env.NODE_ENV;
+
+module.exports = {
+
+  module: {
+
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+        // this will apply to both plain `.js` files
+        // AND `<script>` blocks in `.vue` files
+        {
+          test: /\.js$/,
+          loader: 'babel-loader'
+        },
+        // this will apply to both plain `.css` files
+        // AND `<style>` blocks in `.vue` files
+        {
+          test: /\.css$/,
+          use: [
+            'vue-style-loader',
+            'css-loader'
+          ]
+        }
+      ]
+    },
+  
+
+
+    rules: [
+      // ... other rules
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    // make sure to include the plugin!
+    new VueLoaderPlugin('./src/audio/audio.wav')
+  ],
+
+    module: {
+  loaders: [
+
+    {
+      test: /\.php$/,
+      loaders: [
+        'html-minify',
+        'php-loader'
+      ]
+    },
+ 
+    ]
+},
+
+  entry: './app/index.js',
+
+  mode: env,
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/'
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 3500,
+    hot: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          env == 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    
+    new HtmlWebpackPlugin({template: './app/index.html'
+  }),
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new FaviconsWebpackPlugin('./public/favicon.png')
+  ]
+};
